7310f49a51e44bd5d15d59f59f6f989b533dac9f
Charlie White <charlie.white@lafleur.marketing>
2025-03-26 11:59:17 -0400

    Change to Github

--- lafleur-settings-plugin.php
+++ lafleur-settings-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LaFleur Settings Plugin
  * Description: Configure common settings for LaFleur Hosting
- * Version: 1.1.6
+ * Version: 1.1.7
  * Author: LaFleur Marketing
  */
 
